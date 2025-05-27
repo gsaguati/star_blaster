2fe2861e14db5a40b821abf76e93c27bb5af2eb9
gsaguati <gsaguati@gmail.com>
2025-05-27 09:50:59 -0300

    .

--- player.py
+++ player.py
@@ -7,12 +7,12 @@ class Player:
         self.rect = pygame.Rect(x, y, 33, 33)
         self.speed = 6
 
-    def movimiento(self, keys, screen_width):
+    def movimiento(self, keys, width):
      if keys[pygame.K_LEFT]:
         if self.rect.left > 0:
             self.rect.x -= self.speed
      elif keys[pygame.K_RIGHT]:
-        if self.rect.right < screen_width:
+        if self.rect.right < width:
             self.rect.x += self.speed
 
     def draw(self, screen):
