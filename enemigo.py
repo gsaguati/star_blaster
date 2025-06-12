96067d4bc58e74212e135a0ebffa442779b0af98
gsaguati <gsaguati@gmail.com>
2025-06-12 10:16:23 -0300

    enemigo

---
+++ enemigo.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Enemigo:
+    def __init__(self, x, y, imagen):
+        self.imagen = imagen
+        self.rect = pygame.Rect(x, y, 40, 40)
+        self.velocidad_x = 2
+        self.velocidad_y = 20
+        self.direccion = 1 
+
+    def mover(self, width):
+        self.rect.x += self.velocidad_x * self.direccion
+        if self.rect.right >= width or self.rect.left <= 0:
+            self.direccion *= -1
+            self.rect.y += self.velocidad_y
+
+    def draw(self, pantalla):
+        pantalla.blit(self.imagen, self.rect)
+        pygame.draw.rect(pantalla, (0, 0, 255), self.rect)  
--- juego.py
+++ juego.py
@@ -1,25 +1,32 @@
 import pygame
 from player import Player
 from bala import Bala
+from enemigo import Enemigo
+import random
 
 def jugar():
     pygame.init()
-
-    nave_img = pygame.image.load("assets/img/nave1.png")
-    nave = Player(200, 500, nave_img)
-
     width = 450
     height = 600
     ventana = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Star Blaster")
 
+    nave_img = pygame.image.load("assets/img/nave1.png")
+    enemigo_img = pygame.image.load("assets/img/enemigo1.png")
+
+    nave = Player(200, 500, nave_img)
     balas = []
-    bala_delay = 300  
-    ultima_bala = pygame.time.get_ticks()  
+    enemigos = []
+
+    bala_delay = 600
+    ultima_bala = pygame.time.get_ticks()
+    tiempo_oleada = pygame.time.get_ticks() - 5000
+    intervalo_oleada = 4000
 
     run = True
     while run:
         pygame.time.delay(30)
+        ahora = pygame.time.get_ticks()
         keys = pygame.key.get_pressed()
 
         for event in pygame.event.get():
@@ -28,12 +35,23 @@ def jugar():
 
         nave.movimiento(keys, width)
 
-        ahora = pygame.time.get_ticks()
         if keys[pygame.K_SPACE]:
             if ahora - ultima_bala > bala_delay:
                 bala = Bala(nave.rect.centerx - 2, nave.rect.top)
                 balas.append(bala)
                 ultima_bala = ahora
+
+        if ahora - tiempo_oleada > intervalo_oleada:
+            cantidad_enemigos = random.randint(3, 6)
+            for i in range(cantidad_enemigos):
+                x = random.randint(30, width - 70)
+                y = random.randint(-20, 10)
+                enemigo = Enemigo(x, y, enemigo_img)
+                enemigo.velocidad_x = random.uniform(1.5, 3.5)
+                enemigo.velocidad_y = random.uniform(35, 55)
+                enemigo.direccion = random.choice([-1, 1])
+                enemigos.append(enemigo)
+            tiempo_oleada = ahora
 
         ventana.fill((0, 0, 0))
         nave.draw(ventana)
@@ -43,6 +61,15 @@ def jugar():
             bala.draw(ventana)
             if bala.rect.bottom < 0:
                 balas.remove(bala)
+
+        for enemigo in enemigos[:]:
+            enemigo.mover(width)
+            enemigo.draw(ventana)
+            for bala in balas[:]:
+                if bala.rect.colliderect(enemigo.rect):
+                    enemigos.remove(enemigo)
+                    balas.remove(bala)
+                    break
 
         pygame.display.update()
 
